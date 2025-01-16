ff888af23ef0ca798e50d5a626a9d383ac4ff8f5
john9francis <john9francis@gmail.com>
2025-01-16 11:02:36 -0500

    Started infrastructure for database access

--- README.md
+++ README.md
@@ -9,3 +9,7 @@ This app will allow you to:
 2. View all contacts
 3. Edit contact by either editing their name or phone number
 4. Delete a contact
+
+
+# Useful websites
+- [Writing platform specific code](https://docs.flutter.dev/platform-integration/platform-channels)
---
+++ lib/db_access.dart
@@ -0,0 +1,24 @@
+
+
+
+class DbAccess {
+  bool connect(){
+    return false;
+  }
+
+  bool create(String name, int number){
+    return false;
+  }
+
+  String read(int howMany){
+    return "";
+  }
+
+  bool update(String name, String newName, int newNumber){
+    return false;
+  }
+
+  bool delete(String name){
+    return false;
+  }
+}
